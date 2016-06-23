63538fd9532d91f25aaf65557c77b1163570304d
Dominic Serrano <dmsrojo@gmail.com>
2016-06-23 11:26:16 -0500

    Added basic html for ui and styling. Began to set up js functionality plan

---
+++ src/math.js
@@ -0,0 +1,39 @@
+
+//create reference to input
+
+
+
+
+//one function that handles inputs and passes input and function to operator function, takes 3 args
+
+
+//event listener for enter key, calls function and completes operation chosen
+
+
+
+//operator functions
+function add( one, two ) {
+  return one + two;
+}
+
+function subtract( one, two ) {
+  return one - two;
+}
+
+function multiply( one, two ) {
+  return one * two;
+}
+
+function divide( one, two ) {
+  return one / two;
+}
+
+function square( one ) {
+  return one * one;
+}
+
+function squareRoot( one ) {
+  return Math.sqrt(one);
+}
+
+console.log('hello')
