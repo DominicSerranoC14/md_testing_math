0e722b3df785d3180e42e8fbd8a7bf5764afb3f7
Dominic Serrano <dmsrojo@gmail.com>
2016-06-23 14:50:15 -0500

    Successfully completed user interaction, all operations work

--- src/math.js
+++ src/math.js
@@ -1,39 +1,66 @@
 
 //create reference to input
+var userInput = $('#user-input');
+var op;
+// console.log(userInput);
+
+//grabs first integer and operator
+var grabOp = function() {
+
+  // when a button is clicked the user input and operation selected is passed to another function which operates the function
+  $('.op').click( function(each) {
+    var num1 = parseInt(userInput.val());
+    op = $(this).html();
+    console.log(op, num1)
+    runOp(op, num1);
+
+    //sets text box back to an empty string on any click
+    userInput.val("");
+  });
+}//end grabOp
 
 
+//operates on enter press
+var runOp = function(op, num1) {
 
-
-//one function that handles inputs and passes input and function to operator function, takes 3 args
-
-
-//event listener for enter key, calls function and completes operation chosen
-
+  //event listener that listens for enter key
+  $('#user-input').keyup( function() {
+    //test is enter was pressed then function
+    if ( event.keyCode === 13 ) {
+      let num2 = parseInt(userInput.val());
+      // console.log(typeof(op))
+      console.log(op)
+      userInput.val(window[op](num1, num2));
+    }
+  });
+}////////end runOp
 
 
 //operator functions
-function add( one, two ) {
+var add = function( one, two ) {
   return one + two;
 }
 
-function subtract( one, two ) {
+var subtract = function( one, two ) {
   return one - two;
 }
 
-function multiply( one, two ) {
+var multiply= function( one, two ) {
   return one * two;
 }
 
-function divide( one, two ) {
+var divide = function( one, two ) {
   return one / two;
 }
 
-function square( one ) {
+var square = function( one ) {
   return one * one;
 }
 
-function squareRoot( one ) {
+var squareRoot = function( one ) {
   return Math.sqrt(one);
 }
 
-console.log('hello')
+
+
+grabOp();
